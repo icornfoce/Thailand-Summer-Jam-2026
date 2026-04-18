a793bbce3c2f61763df475abcb72d87c6f47c02f
IamArmsDev <thadapongsa2552@gmail.com>
2026-04-18 21:37:21 +0700

    update enemyHP

---
+++ Assets/script/Enemy/EnemyHP.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage. Current HP: {currentHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{gameObject.name} died!");
+        // Add death logic here (e.g., play animation, spawn particles, drop loot)
+        Destroy(gameObject);
+    }
+}
